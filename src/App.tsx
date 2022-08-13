8620ecd3ac4472f49bac9ba1366761d08b7574f4
Dan Wahlin <you@example.com>
2022-08-12 18:39:18 -0700

    Move to React

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search from './components/search';
+
+function App() {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/search/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Search() {
+  return (
+    <div>
+      {/* <div className="search-container">
+         Search: <input type="text" title="Search" />
+         <div>
+          <div>Checkboxes</div>
+          <div>Results</div>
+         </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default Search;
