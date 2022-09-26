468c8c220bf478402e2fae42d6b70d95b06e02c4
Dan Wahlin <you@example.com>
2022-09-26 15:10:34 -0700

    Add useAzureCommunicationCallAdapter

--- samples/acs-to-teams-meeting/client/react/src/App.tsx
+++ samples/acs-to-teams-meeting/client/react/src/App.tsx
@@ -1,170 +1,60 @@
-import { AzureCommunicationTokenCredential } from '@azure/communication-common';
-import {
-  CallAdapter,
-  CallComposite, createAzureCommunicationCallAdapter,
-} from '@azure/communication-react';
+import { AzureCommunicationTokenCredential, CommunicationUserIdentifier } from '@azure/communication-common';
+import {  CallComposite, fromFlatCommunicationIdentifier, useAzureCommunicationCallAdapter } from '@azure/communication-react';
 import React, { useState, useMemo, useEffect } from 'react';
 import './App.css';
 
 const App = () => { 
-
+  const displayName = 'Guest'
   const userId = process.env.REACT_APP_ACS_USER_ID as string;
   const token = process.env.REACT_APP_ACS_TOKEN as string;
-  const [teamsMeetingLink, setTeamsMeetingLink] = useState('');
+  const [teamsMeetingLink, setTeamsMeetingLink] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
   const credential = useMemo(() => new AzureCommunicationTokenCredential(token), [token]);
-  const [adapter, setAdapter] = useState<CallAdapter | undefined>();
+
+  const callAdapterArgs = useMemo(() => 
+    ({
+      userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
+      displayName,
+      credential,
+      locator: { meetingLink: teamsMeetingLink },
+    }),
+    [userId, credential, displayName, teamsMeetingLink]
+  );
+
+  const callAdapter = useAzureCommunicationCallAdapter(callAdapterArgs);
 
   useEffect(() => {
     const getTeamsMeetingLink = async () => {
+      setMessage('Getting Teams meeting link...');
       //Call Azure Function to get the meeting link
       let res = await fetch(process.env.REACT_APP_FUNCTION_URL as string);
       let link = await res.text();
       setTeamsMeetingLink(link);
+      setMessage('');
       console.log('Teams meeting link', link);
     }
     getTeamsMeetingLink();
   }, []);
 
-  useEffect(() => {
-    const createCallAdapter = async () => {
-      const args = {
-        userId: { communicationUserId: userId },
-        displayName: 'Guest',
-        credential,
-        locator: { meetingLink: teamsMeetingLink }
-      };
-      const adapter = await createAzureCommunicationCallAdapter(args);
-      setAdapter(adapter);
-    }
-
-    if (userId && teamsMeetingLink && credential) {
-      createCallAdapter();
-    }
-  }, [userId, teamsMeetingLink, credential]); // When all have values create the call adapter
-
-  if (adapter) {
+  if (callAdapter) {
     return (
       <div>
         <h1>Contact Customer Service</h1>
         <div className="wrapper">
           <CallComposite
-            adapter={adapter}
+            adapter={callAdapter}
           />
         </div>
       </div>
     );
   }
-  if (credential === undefined) {
+  if (!credential) {
     return <>Failed to construct credential. Provided token is malformed.</>;
   }
-  return <>Initializing...</>;
+  if (message) {
+    return <div>{message}</div>;
+  }
+  return <div>Initializing...</div>;
 };
 
 export default App;
-
-// import React, { useEffect, useState } from 'react';
-// import { Call, CallAgent, CallClient, Features } from '@azure/communication-calling';
-// import { AzureCommunicationTokenCredential } from '@azure/communication-common';
-// import './App.css';
-
-// const App = () => {
-//   // Represents the ACS --> Teams Meeting call and call state
-//   const [call, setCall] = useState<Call>();
-//   const [callState, setCallState] = useState('--');
-
-//   // Represents the call agent to allow us to join a call
-//   const [callAgent, setCallAgent] = useState<CallAgent>();
-
-//   // Handle UI states
-//   const [callInProgress, setCallInProgress] = useState(false);
-//   const [teamsMeetingLink, setTeamsMeetingLink] = useState<string>();
-//   const [recordingState, setRecordingState] = useState('');
-
-//   const getTeamsMeetingLink =  async () => {
-//     //Call Azure Functions to get the meeting link
-//     setCallState('Getting meeting information...');
-//     let res = await fetch('http://localhost:7071/api/TeamsMeetingFunction');
-//     let text = await res.text();
-//     console.log('Teams meeting link', text);
-//     setCallState('Meeting information retrieved...');
-//     //Set the meeting link
-//     setTeamsMeetingLink(text);
-//   };
-
-//   const hangup = async () => {
-//     if (call) {
-//       await call.hangUp();
-//       setCallState('--');
-//       setCallInProgress(false);
-//     }
-//   }
-
-//   useEffect(() => {
-//     const joinTeamsMeeting = async () => {
-//       // Join the call
-//       if (callAgent && teamsMeetingLink) {
-//         const currentCall = callAgent.join({ meetingLink: teamsMeetingLink }, {});
-//         setCall(currentCall);
-//       }
-//     }
-//     joinTeamsMeeting();
-//   }, [callAgent, teamsMeetingLink]);
-
-//   useEffect(() => {
-//     if (call) {
-//       call.on('stateChanged', () => {
-//         setCallState(call.state);
-//       });
-//       call.feature(Features.Recording).on('isRecordingActiveChanged', () => {
-//         if (call.feature(Features.Recording).isRecordingActive) {
-//             setRecordingState('This call is being recorded');
-//         }
-//         else {
-//           setRecordingState('');
-//         }
-//       });
-//       setCallInProgress(true);
-//     }
-//   } , [call]);
-
-//   useEffect(() => {
-//     const init = async() => {
-//       const callClient = new CallClient();
-//       // Get a token from the Azure Portal. Go to ACS Resource --> `Identities & User Access Tokens`, select the
-//       // Voice and video calling (VOIP) checkbox and copy the User Access token value below
-//       const tokenCredential = new AzureCommunicationTokenCredential(process.env.REACT_APP_ACS_TOKEN as string);
-//       try {
-//         const agent = await callClient.createCallAgent(tokenCredential, { displayName: 'ACS External User' });
-//         setCallAgent(agent);
-//       }
-//       catch (error) {
-//         console.log(error);
-//       }
-//     };
-
-//     init();
-
-//   } , []);
-
-//   return (
-//     <div className="App">
-//       <h1>Using Azure Communication Services to Call into a Teams Meeting</h1>
-//       {/* <h3>Teams meeting link</h3>
-//       <button type="button" onClick={getTeamsMeetingLink}>Talk to an agent</button>
-//       <br /><br /> */}
-//       <div className="button-container">
-//         <button type="button" disabled={callInProgress} onClick={getTeamsMeetingLink}>
-//           Join Meeting
-//         </button>
-//         <button type="button" disabled={!callInProgress} onClick={hangup}>
-//           Hang Up
-//         </button>
-//       </div>
-//       <br />
-//       <div><span>{recordingState}</span></div>
-//       <div>Call state <span>{callState}</span></div>
-//     </div>
-//   );
-// }
-
-// export default App;
