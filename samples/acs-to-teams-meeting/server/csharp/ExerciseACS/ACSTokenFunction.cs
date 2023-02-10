0620457a661bb60c46d04d89e04aa03d3da638a8
Kazuki Ota <kaota@microsoft.com>
2023-02-10 17:52:16 +0900

    remove POST method

--- samples/acs-to-teams-meeting/server/csharp/ExerciseACS/ACSTokenFunction.cs
+++ samples/acs-to-teams-meeting/server/csharp/ExerciseACS/ACSTokenFunction.cs
@@ -24,7 +24,7 @@ public class ACSTokenFunction
 
     [FunctionName("ACSTokenFunction")]
     public async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
         ILogger log)
     {
         var user = await _tokenClient.CreateUserAsync();
