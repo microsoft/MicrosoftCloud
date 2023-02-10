7da69ff8f4fa8c47241a061c8a7343a7f4d053e5
Kazuki Ota <kaota@microsoft.com>
2023-02-10 17:46:54 +0900

    refactor to use DI and update to C# 10

--- samples/acs-to-teams-meeting/server/csharp/ExerciseACS/TeamsMeetingFunction.cs
+++ samples/acs-to-teams-meeting/server/csharp/ExerciseACS/TeamsMeetingFunction.cs
@@ -1,43 +1,55 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Azure.Identity;
 using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
 
 namespace ExerciseACS;
 
-public static class TeamsMeetingFunction
+public class TeamsMeetingFunction
 {
+    private readonly GraphServiceClient _graphServiceClient;
+    private readonly IConfiguration _configuration;
+
+    public TeamsMeetingFunction(GraphServiceClient graphServiceClient, IConfiguration configuration)
+    {
+        _graphServiceClient = graphServiceClient;
+        _configuration = configuration;
+    }
+
     [FunctionName("TeamsMeetingFunction")]
-    public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
         ILogger log)
     {
-        var clientSecretCredential = new ClientSecretCredential(
-            Environment.GetEnvironmentVariable("TENANT_ID"),
-            Environment.GetEnvironmentVariable("CLIENT_ID"),
-            Environment.GetEnvironmentVariable("CLIENT_SECRET")
-        );
+        var userId = _configuration.GetValue<string>("USER_ID");
+        var newEvent = await _graphServiceClient
+            .Users[userId]
+            .Calendar
+            .Events
+            .Request()
+            .AddAsync(CreateMeetingEvent());
 
-        var graphServiceClient = new GraphServiceClient(
-            clientSecretCredential,
-            new[] { "https://graph.microsoft.com/.default" }
-        );
-
-        var userId = Environment.GetEnvironmentVariable("USER_ID");
-        var newEvent = await graphServiceClient.Users[userId].Calendar.Events.Request().AddAsync(new()
-        {
-            Subject = "Customer Service Meeting",
-            Start = new() { DateTime = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss"), TimeZone = "UTC" },
-            End = new() { DateTime = DateTime.UtcNow.AddHours(1).ToString("yyyy-MM-ddTHH:mm:ss"), TimeZone = "UTC" },
-            IsOnlineMeeting = true
-        });
         return new OkObjectResult(newEvent.OnlineMeeting.JoinUrl);
     }
+
+    private static Event CreateMeetingEvent() => new()
+    {
+        Subject = "Customer Service Meeting",
+        Start = new() 
+        { 
+            DateTime = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss"), 
+            TimeZone = "UTC" 
+        },
+        End = new() 
+        { 
+            DateTime = DateTime.UtcNow.AddHours(1).ToString("yyyy-MM-ddTHH:mm:ss"), 
+            TimeZone = "UTC" 
+        },
+        IsOnlineMeeting = true
+    };
 }
