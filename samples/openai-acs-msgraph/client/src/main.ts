9508c3ea76aea6ebc246de9d21c04e9d9ab9d4e9
danwahlin <dwahlin@xmlforasp.net>
2023-05-20 12:16:25 -0700

    Remove module

--- samples/openai-acs-msgraph/client/src/main.ts
+++ samples/openai-acs-msgraph/client/src/main.ts
@@ -1,6 +1,5 @@
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { OverlayModule } from './app/core/overlay/overlay.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -10,21 +9,27 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { withInterceptorsFromDi, provideHttpClient, withInterceptors, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { PhonePipe } from './app/shared/phone.pipe';
 import { CurrencyPipe, DatePipe, DecimalPipe, TitleCasePipe } from '@angular/common';
+import { OverlayRequestResponseInterceptor } from './app/core/overlay/overlay-request-response.interceptor';
 
 
 bootstrapApplication(AppComponent, {
     providers: [
         importProvidersFrom(BrowserModule, FormsModule, MatBadgeModule, MatButtonModule, MatCardModule, 
-          MatDialogModule, MatExpansionModule, MatMenuModule, MatTabsModule, MatToolbarModule, MatIconModule, OverlayModule),
+          MatDialogModule, MatExpansionModule, MatMenuModule, MatTabsModule, MatToolbarModule, MatIconModule),       
         CurrencyPipe, DatePipe, DecimalPipe, PhonePipe, TitleCasePipe,
         provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi())
+        provideHttpClient(withInterceptorsFromDi()),
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: OverlayRequestResponseInterceptor,
+          multi: true,
+        }
     ]
-})
+  })
   .catch(err => console.error(err));
